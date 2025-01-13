03d5e06db374a172a2cb9cd78695407bb6dea1bf
Sangamkumar24 <132844267+Sangamkumar24@users.noreply.github.com>
2025-01-13 07:30:24 +0000

    variables added

---
+++ 01_basics/variables.js
@@ -0,0 +1,10 @@
+let accontState;
+const accountType="Student";
+let accountName= "Sangam"
+var password= "12890"
+
+accountNo= 12345 //in js variable can also be declared as this;
+
+console.table([accontState, accountName, accountType, password, accountNo]);
+
+//never use var because of scope block problem
