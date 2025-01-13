0d40b1fc014b28a46c5f01faee76f34fd27c7b75
Sangamkumar24 <132844267+Sangamkumar24@users.noreply.github.com>
2025-01-13 18:24:09 +0000

    added extension prettier

--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -7,4 +7,4 @@ accountNo= 12345 //in js variable can also be declared as this;
 
 console.table([accontState, accountName, accountType, password, accountNo]);
 
-//never use var because of scope block problem
+//never use var because of block scope and functional scope  problem
