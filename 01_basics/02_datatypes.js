2ce4e87635c8e8ea811911fa1f0eb2fcb05787e8
Sangamkumar24 <132844267+Sangamkumar24@users.noreply.github.com>
2025-02-07 17:50:08 +0000

    02 datatypes added

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // helps traet all js code as per new version
+
+//alert("Hi"); // alert will not work as we are on node not on browser
+
+// console.log("hi")
+
+//code readability should be high 
+
+let fname="Sangam"   //type string
+let age=22          // type number
+let isLoggedIn= false  // type boolean
+let game;
+
+console.log("game ", typeof(game)) // here type and value of game is undefined
+
+//Number - 2^53
+//BigInt- for very big numbers
+//string
+//Boolean- true/false
+//null- standalone value
+//undefined
+//Symbol- unique
+
+//object
+
+console.log( typeof(undefined)); //undefined
+console.log( typeof(null)); //object
