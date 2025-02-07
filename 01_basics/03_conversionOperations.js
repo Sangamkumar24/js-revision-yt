9f1e1a24343ecf31f4fa469ead33bcc4eee1f8ac
Sangamkumar24 <132844267+Sangamkumar24@users.noreply.github.com>
2025-02-07 18:14:17 +0000

    03 added

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,9 @@
+let score= "Sangam"
+
+// console.log(score)
+// console.log("type of score", typeof(score));
+
+let valueInNumber= Number(score)
+
+console.log("type after conversion ", typeof valueInNumber); //number
+console.log(valueInNumber) //NaN (Not a number)
