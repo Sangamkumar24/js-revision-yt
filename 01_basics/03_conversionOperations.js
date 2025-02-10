af9454f5d3a4054114469070b7309dcfc233470e
Sangamkumar24 <132844267+Sangamkumar24@users.noreply.github.com>
2025-02-10 08:08:18 +0000

    03 modified

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -7,3 +7,27 @@ let valueInNumber= Number(score)
 
 console.log("type after conversion ", typeof valueInNumber); //number
 console.log(valueInNumber) //NaN (Not a number)
+
+// for the above conversion here are the following results for the given values
+// "33" --- 33
+//"33abc" ---- NaN
+//true-1 & false-0
+
+// for boolean conversion
+// "string"== true
+//"" == 0
+
+//we can merge two strings just by add operator str1+str2
+
+console.log("1"+ 2);//12
+console.log(1 + "2");//12
+console.log("1"+2+2);//122
+console.log(1+2+"3"); //33
+
+//it is preferred to use brackets for getting expected output as per required logic
+
+console.log(+true); // 1
+console.log(+"");// 0
+
+
+
