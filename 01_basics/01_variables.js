8ae075f87d5b56c119c21005a5e67e9ebf5b8b0a
Sangamkumar24 <132844267+Sangamkumar24@users.noreply.github.com>
2025-06-19 15:52:37 +0000

    changes in variables basics

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -5,6 +5,9 @@ var password= "12890"
 
 accountNo= 12345 //in js variable can also be declared as this;
 
+// variable assigned without any declaration keyword. In JavaScript, 
+// this implicitly creates a global variable (not recommended)
+
 console.table([accontState, accountName, accountType, password, accountNo]);
 
 //never use var because of block scope and functional scope  problem
